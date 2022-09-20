aea5ed2d86ea758d17f45b58b1c56ebf419e794a
XuanHieu <hoxuanhieu.pch.2b1.92@gmail.com>
2022-09-20 14:32:41 +0700

    [220920] [update] - three with vite

---
+++ examples/220920-material/main.js
@@ -0,0 +1,49 @@
+import * as THREE from "three"
+
+// loader texture
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('./res/diamond.jpg')
+texture.minFilter = THREE.NearestFilter
+texture.magFilter = THREE.NearestFilter
+
+const { innerWidth, innerHeight } = window
+const W = innerWidth
+const H = innerHeight
+const ASPECT = W/H
+const scene = new THREE.Scene()
+const container = document.getElementById("stage")
+const camera = new THREE.PerspectiveCamera(75,ASPECT,0.1,100)
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: container})
+const geometry = new THREE.BoxGeometry(5,5,5)
+const material = new THREE.MeshStandardMaterial({
+  wireframe: false,
+  map: texture
+})
+const mesh = new THREE.Mesh(geometry,material)
+const ambientLight = new THREE.AmbientLight(0xffffff)
+const directionLight = new THREE.DirectionalLight(0xfffff,1 )
+
+
+
+
+scene.background = new THREE.Color("black")
+camera.position.z = 10
+renderer.setSize(W,H)
+
+scene.add(mesh,ambientLight,directionLight)
+
+
+const animate = () => {
+  renderer.render(scene,camera)
+  window.requestAnimationFrame(animate)
+
+  mesh.rotation.z += 0.01
+  mesh.rotation.y += 0.01
+}
+
+animate()
+
+
+
+
+
--- examples/220920-start/scirpt.js
+++ examples/220920-start/scirpt.js
@@ -1,20 +1,37 @@
+const vertices = [] 
 const scene = new THREE.Scene()
 console.log(scene)
 const container = document.querySelector("#webgl")
-scene.background = new THREE.Color(0x00ff00)
+scene.background = new THREE.Color(0x000000)
 console.log(container)
 
 const camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000)
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: container })
 renderer.setSize(window.innerWidth,window.innerHeight)
-camera.position.z = 5 
+camera.position.z = 100
 
-const geometry = new THREE.BoxGeometry(2,2,2)
-const material = new THREE.MeshStandardMaterial({ color: 0xffff00 })
-const mesh = new THREE.Mesh(geometry, material)
+// const geometry = new THREE.BoxGeometry(2,2,2, 12, 20, 20)
+const geometry = new THREE.BufferGeometry()
+
+const numberPoints = 500
+for(i =0 ; i<numberPoints;i++){
+    const x = THREE.MathUtils.randFloatSpread(100)
+    const y = THREE.MathUtils.randFloatSpread(100)
+    const z = THREE.MathUtils.randFloatSpread(00)
+    vertices.push(x,y,z) 
+}
+geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices,3))
+
+
+const material = new THREE.PointsMaterial({ 
+    size: 1,
+    color: 0xffffff,
+    wireframe: true,
+})
+const mesh = new THREE.Points(geometry, material)
 scene.add(mesh)
 
-const ambientLight = new THREE.AmbientLight(0x404040)
+const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(ambientLight)
 const directionLight = new THREE.DirectionalLight(0x404040, 1)
 scene.add(directionLight)
@@ -23,17 +40,19 @@ scene.add(directionLight)
 // roll: zoom in, zoom out; leftMouse: move camera around object; rightMouse: move camera to another space
 // dom is document object model 
 const orbitControl = new THREE.OrbitControls(camera, renderer.domElement)
+
+
+
+
 function animate() {
     renderer.render(scene,camera)
 
-    mesh.rotation.y +=0.01
-    mesh.rotation.z +=0.01
+    // mesh.rotation.y +=0.01
+    // mesh.rotation.z +=0.01
 
     orbitControl.update()
     window.requestAnimationFrame(animate)
 }
-
-
 
 
 animate()
