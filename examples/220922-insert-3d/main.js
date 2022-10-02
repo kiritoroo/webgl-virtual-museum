07008d1a5c87912949de32400344395cb718215c
XuanHieu <hoxuanhieu.pch.2b1.92@gmail.com>
2022-10-02 22:24:08 +0700

    [221002] [godrays] - example godrays

--- examples/220922-insert-3d/main.js
+++ examples/220922-insert-3d/main.js
@@ -2,8 +2,11 @@ import * as THREE from "three"
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from "three/examples/jsm/loaders/gltfloader"
 import { DRACOLoader } from "three/examples/jsm/loaders/dracoloader"
-
-
+//import { GodRaysFakeSunShader, GodRaysDepthMaskShader, GodRaysCombineShader, GodRaysGenerateShader } from 'three/addons/shaders/GodRayShader.js';
+//import Stats from 'three/addons/libs/stats.module.js'
+import { Camera } from "three";
+import { GodRaysFakeSunShader, GodRaysDepthMaskShader, GodRaysCombineShader, GodRaysGenerateShader } from 'three/examples/jsm/shaders/GodRaysShader';
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const {innerWidth, innerHeight} = window
 const W = innerWidth
@@ -16,6 +19,7 @@ const container = document.getElementById("stage")
 document.body.appendChild(container)
 // create camera
 const camera = new THREE.PerspectiveCamera(75,ASPECT,0.1,100)
+
 // create renderer with antilias, canvas
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas: container})
 // create controller
@@ -41,132 +45,267 @@ renderer.setPixelRatio(devicePixelRatio)
 scene.add(ambientLight,directionLight)
 
 
-// let mesh2
-// // array to store 3d models
-// đòi dracoLoader
-// const loader2 = new GLTFLoader()
-// loader2.load('./models/dna.glb',
-//   (file) => {
-//     console.log(file)
-//     mesh2 = file.scene.children[0]
-//     mesh2.position.set(5,6,2)
-//     scene.add(mesh2)
-//   },
+//
+let mesh, stats
+const loader = new GLTFLoader()
 
-//   (xhr) =>{
-//     console.log((xhr.loaded / xhr.total * 100) + '% loaded')
-//   },
+loader.load('./models/parrot.glb',
+  (file) =>{
+    console.log(file)
+    mesh = file.scene.children[0]
+    mesh.position.set(0.5,0.1,0.1)
+    scene.add(mesh)   
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total *100) + '% loaded')
+  },
 
-//   (error) => {
-//     console.log('Have problem' + error)
-//   }
-// )
+  (error) => {
+    console.log('Have a problem')
+  }
+)
 
-// let mesh1
+// -----------------------------
 
-// const loader1 = new GLTFLoader()
-// loader1.load('./models/xss.glb',
-//   (file) => {
-//     console.log(file)
-//     mesh1 = file.scene.children[0]
-//     mesh1.position.set(0.1,4,2)
-//     scene.add(mesh1)
-//   },
+let sphereMesh, materialDepth
 
-//   (xhr) =>{
-//     console.log((xhr.loaded / xhr.total * 100) + '% loaded')
-//   },
+const sunPosition = new THREE.Vector3(0,1000,-1000)
+const clipPosition = new THREE.Vector4();
+const screenSpacePosition = new THREE.Vector3()
 
-//   (error) => {
-//     console.log('Have problem' + error)
-//   }
-// )
+const postProcessing = { enabled: true}
 
-// let mesh
-// const loader = new GLTFLoader()
+const orbitRadius = 200
 
-// loader.load('./models/parrot.glb',
-//   (file) =>{
-//     console.log(file)
-//     mesh = file.scene.children[0]
-//     mesh.position.set(0.5,0.1,0.1)
-//     scene.add(mesh)   
-//   },
-//   (xhr) => {
-//     console.log((xhr.loaded / xhr.total *100) + '% loaded')
-//   },
+const bgColor = 0x000511
+const sunColor = 0xffee00
 
-//   (error) => {
-//     console.log('Have a problem')
-//   }
-// )
+const godrayRenderTargetResolutionMultiplier = 1.0/4.0
+
+materialDepth = new THREE.MeshDepthMaterial()
+
+const materialScene = new THREE.MeshBasicMaterial({color: 0x000000})
 
 
-function setupModel(data) {
-  const model = data.scene.children[0]
-  //scene.add(model)
-  return model
+// sphere
+const geo = new THREE.SphereGeometry(1,20,10)
+sphereMesh = new THREE.Mesh(geo, materialScene)
+sphereMesh.scale.multiplyScalar(20)
+scene.add(sphereMesh)
+
+
+stats = new Stats()
+container.appendChild(stats.dom)
+
+
+
+window.addEventListener('resize', onWindowResize)
+initPostprocessing(W,H)
+
+
+
+function onWindowResize() {
+
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(W,H)
+  postProcessing.rtTextureColors.setSize(W,H)
+  postProcessing.rtTextureDepth.setSize(W,H)
+  postProcessing.rtTextureDepthMask.setSize(W,H)
+  
+  const adjustedWidth = W * godrayRenderTargetResolutionMultiplier
+  const adjustedHeight = H * godrayRenderTargetResolutionMultiplier
+  postProcessing.rtTextureGodRays1.setSize(adjustedWidth,adjustedHeight)
+  postProcessing.rtTextureGodRays2.setSize(adjustedWidth,adjustedHeight)
+
 }
 
-async function loadModels() {
-  const loader = new GLTFLoader();
-  const dracoLoader = new DRACOLoader()
-  dracoLoader.setDecoderPath("/public/draco/")
-  loader.setDRACOLoader(dracoLoader)
+function initPostprocessing(renderTargetWidth, renderTargetHeight) {
+  postProcessing.scene = new THREE.Scene()
 
-  const [parrotData, xssData, dnaData, roomData ] = await Promise.all([
-    loader.loadAsync('/public/models/parrot.glb'),
-    loader.loadAsync('/public/models/xss.glb'),
-    loader.loadAsync('/public/models/dna.glb'),
-    loader.loadAsync('/public/models/final-room-v2.glb'),
-  ]); 
+  postProcessing.camera = new THREE.OrthographicCamera(-0.5,0.5,0.5,-0.5,-10000,10000)
+  postProcessing.camera.position.z = 100
 
-  const parrot = setupModel(parrotData);
-  parrot.position.set(0, 0, 2.5);
+  postProcessing.scene.add(postProcessing.camera)
 
-  const xss = setupModel(xssData);
-  xss.position.set(7.5, 0, -10);
+  postProcessing.rtTextureColors = new THREE.WebGLRenderTarget(renderTargetWidth, renderTargetHeight)
+  postProcessing.rtTextureDepth = new THREE.WebGLRenderTarget(renderTargetWidth,renderTargetHeight)
 
-  const dna = setupModel(dnaData);
-  dna.position.set(0, -2.5, -10);
+  const adjustedWidth = renderTargetWidth * godrayRenderTargetResolutionMultiplier
+  const adjustedHeight = renderTargetHeight *godrayRenderTargetResolutionMultiplier
 
-  const room = setupModel(roomData)
-  room.position.set(5,6,7)
+  postProcessing.rtTextureGodRays1 = new THREE.WebGLRenderTarget(adjustedWidth,adjustedHeight)
+  postProcessing.rtTextureGodRays2 = new THREE.WebGLRenderTarget(adjustedWidth,adjustedHeight)
 
-  return {
-    parrot,
-    xss,
-    dna,
-    room,
-  };
-}
 
-let meshs
-
-loadModels()
-  .then(res => {
-    meshs = res
-    console.log(meshs);
-
-    scene.add(meshs.parrot)
-    scene.add(meshs.xss)
-    scene.add(meshs.dna)
-    scene.add(meshs.room)
+  // god ray shaders
+  const godRaysDepthMaskShader = GodRaysDepthMaskShader
+  postProcessing.godrayMaskUniforms = THREE.UniformsUtils.clone(godraysMaskShader.uniforms)
+  postProcessing.materialGodraysDepthMask = new THREE.ShaderMaterial({
+    
+    uniforms: postProcessing.godrayMaskUniforms,
+    vertexShader: godraysMaskShader.vertexShader,
+    fragmentShader: godraysMaskShader.fragmentShader
   })
 
+  const godraysGenShader = GodRaysGenerateShader;
+  postProcessing.godrayGenUniforms = THREE.UniformsUtils.clone(godraysGenShader.uniforms)
+  postProcessing.materialGodraysGenerate = new THREE.ShaderMaterial ({
+    
+    uniforms: postProcessing.godrayGenUniforms,
+    vertexShader: godraysGenShader.vertexShader,
+    fragmentShader: godraysGenShader.fragmentShader
+  })
+
+  const godRaysCombineShader = GodRaysCombineShader
+  postProcessing.godrayCombineUniforms = THREE.UniformsUtils.clone(godRaysCombineShader.uniforms)
+  postProcessing.materialGodraysCombine = new THREE.ShaderMaterial({
+    uniforms: postProcessing.godrayCombineUniforms,
+    vertexShader: godRaysCombineShader.vertexShader,
+    fragmentShader: godRaysCombineShader.fragmentShader
+  })
+
+  const godRaysFakeSunShader = GodRaysFakeSunShader
+  postProcessing.godrayFakeSunUniforms = THREE.UniformsUtils.clone(godRaysFakeSunShader.uniforms)
+  postProcessing.materialGodraysFakeSun = new THREE.ShaderMaterial({
+    uniforms: postProcessing.godrayFakeSunUniforms,
+    vertexShader: godRaysFakeSunShader.vertexShader,
+    fragmentShader: godrayFakeSunUniforms.fragmentShader
+  })
+  
+  postProcessing.godrayFakeSunUniforms.bgColor.value.setHex(bgColor)
+  postProcessing.godrayFakeSunUniforms.sunColor.value.setHex(sunColor)
+
+  postProcessing.godrayCombineUniforms.fGodRayIntensity.value = 0.75
+
+  postProcessing.quad = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.0, 1.0),
+    postProcessing.materialGodraysGenerate
+  )
+
+  postProcessing.quad.position.z = -9900
+  postProcessing.scene.add(postProcessing.quad)
+}
+
+function getStepSize (filterLen, tapsPerPass, pass) {
+  return filterLen * Math.pow(tapsPerPass, - pass)
+}
+
+function filterGodRays(inputTex, renderTarget, stepSize) {
+  postProcessing.scene.overrideMaterial = postProcessing.materialGodraysGenerate
+
+  postProcessing.godrayGenUniforms[ 'fStepSize'].value = stepSize
+  postProcessing.godrayGenUniforms[ 'tInput' ].value = inputTex
+
+  renderer.setRenderTarget(renderTarget)
+  renderer.render(postProcessing.scene, postProcessing.camera)
+  postProcessing.scene.overrideMaterial = null
+}
+
+function render() {
+  const time = Date.now() / 4000
+
+  sphereMesh.position.x = orbitRadius * Math.cos(time)
+  sphereMesh.position.z = orbitRadius * Math.sin(time) -100
+  
+  if (postProcessing.enabled){
+    clipPosition.x = sunPosition.x
+    clipPosition.y = sunPosition.y
+    clipPosition.z = sunPosition.z
+    clipPosition.w = 1
+
+    clipPosition.applyMatrix4(camera.matrixWorldInverse).applyMatrix4(camera.projectionMatrix)
+
+    clipPosition.x /= clipPosition.w
+    clipPosition.y /= clipPosition.w
+
+    screenSpacePosition.x = ( clipPosition.x + 1) /2
+    screenSpacePosition.y = ( clipPosition.y + 1) /2
+    screenSpacePosition.z = clipPosition.z
+
+    postProcessing.godrayGenUniforms[ 'vSunPositionScreenSpace'].value.copy(screenSpacePosition)
+    postProcessing.godrayFakeSunUniforms[ 'vSunPositionScreenSpace'].value.copy(screenSpacePosition)
 
 
+    renderer.setRenderTarget(postProcessing.rtTextureColors)
+    renderer.clear(true, true, true)
 
-const animate = () => {
+    const sunsqH = 0.74 *H
+    const sunsqW = 0.74 *H
+
+    screenSpacePosition.x *=W
+    screenSpacePosition.y *=H
+
+    renderer.setScissor(screenSpacePosition.x-sunsqW/2,screenSpacePosition.y - sunsqH/2, sunsqW, sunsqH)
+    renderer.setScissorTest(true)
+
+    postProcessing.godrayFakeSunUniforms[ 'fAspect'].value = W/H
+    postProcessing.scene.overrideMaterial = postProcessing.materialGodraysFakeSun
+    renderer.setRenderTarget(postProcessing.rtTextureColors)
+    renderer.render(postProcessing.scene, postProcessing.camera)
+
+    renderer.setScissorTest(false)
+
+    // --- draw scene objects ----
+    // colors
+
+    scene.overrideMaterial = null
+    renderer.setRenderTarget(postProcessing.rtTextureColors)
+    renderer.render(scene, camera)
+
+    // depth
+
+    scene.overrideMaterial = materialDepth
+    renderer.setRenderTarget(postProcessing.rtTextureDepth)
+    renderer.clear()
+    renderer.render(scene,camera)
+    //
+    postProcessing.godrayMaskUniforms['tInput'].value = postProcessing.rtTextureDepth.texture
+
+    postProcessing.scene.overrideMaterial = postProcessing.materialGodraysDepthMask
+    renderer.setRenderTarget(postProcessing.rtTextureDepthMask)
+    renderer.render(postProcessing.scene, postProcessing.camera)
+    //---render godrays
+
+    const filterLen = 1.0
+    const TAPS_PER_PASS = 6.0
+
+    // pass 1 - render into first ping-pong targer 
+    filterGodRays(postProcessing.rtTextureDepthMask.texture, postProcessing.rtTextureGodRays2,getStepSize(filterLen,TAPS_PER_PASS,1.0))
+    // pass 2 - render into second ping-pong target
+    filterGodRays(postProcessing.rtTextureGodRays2.texture, postProcessing.rtTextureGodRays1,getStepSize(filterLen, TAPS_PER_PASS,2.0))
+    // pass 3 - 1st RT
+    filterGodRays(postProcessing.rtTextureGodRays1.texture, postProcessing.rtTextureGodRays2,getStepSize(filterLen, TAPS_PER_PASS,3.0))
+    // final pass - composite god-rays onto colors
+    postProcessing.godrayCombineUniforms['tColors'].value = postProcessing.rtTextureColors.texture
+    postProcessing.godrayCombineUniforms['tGodRays'].value = postProcessing.rtTextureGodRays2.texture
+
+    postProcessing.scene.overrideMaterial = postProcessing.materialGodraysCombine
+    renderer.setRenderTarget(null)
+    renderer.render(postProcessing.scene, postProcessing.camera)
+    postProcessing.scene.overrideMaterial = null
+
+  }else {
+    renderer.setRenderTarget(null)
+    renderer.clear()
+    renderer.render(scene,camera)  
+  }
+}
+
+
+function animate() {
+  render()
   renderer.render(scene,camera)
   control.update()
+  
 
-
-  if (meshs) {
-    meshs.parrot.rotation.z += 0.04
-  }
+  // if (mesh) {
+  //   // mesh.parrot.rotation.z += 0.04
+  //   mesh.rotation.z += 0.04
+  // }
 
   window.requestAnimationFrame(animate)
 
 }
+
 animate()
